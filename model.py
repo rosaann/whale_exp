c1c0d9b7ad8c32cf0f9e83a552ff378f692b4900
rosaann <147429240@qq.com>
2019-01-31 16:25:55 +0800

    d

--- model.py
+++ model.py
@@ -66,7 +66,8 @@ class Branch_Model(nn.Module):
         for _ in range(4):
             self.layers += [Sub_Block(512, 128)  ]
         '''     
-        self.layers = nn.ModuleList(self.layers)
+       # self.layers = nn.ModuleList(self.layers)
+        self.layers = nn.Conv2d(32, 64, kernel_size=9, stride=2)
     def forward(self, x):
         print('x ', x.shape)
         out = self.layers(x)  
