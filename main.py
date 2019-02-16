7759f8decec8a214660b2ec938358417d71fd897
rosaann <147429240@qq.com>
2019-02-16 12:05:06 +0800

    d

--- main.py
+++ main.py
@@ -30,14 +30,7 @@ from keras.optimizers import Adam
 from keras.engine.topology import Input
 from keras.layers import Activation, Add, BatchNormalization, Concatenate, Conv2D, Dense, Flatten, GlobalMaxPooling2D, Lambda, MaxPooling2D, Reshape
 from keras.models import Model
-from keras.applications.resnet50 import preprocess_input,ResNet50
-from keras.layers import Dense, GlobalAveragePooling2D
-from keras.layers import LSTM
-from keras.layers import Dense
-from keras.layers import TimeDistributed
-from keras.layers import Bidirectional
-from keras import backend as K
-from keras.utils import Sequence
+
 import keras
 #from tqdm import tqdm_notebook
 from tensorboardX import SummaryWriter
@@ -435,128 +428,10 @@ class FeatureGen(Sequence):
         return (len(self.data) + self.batch_size - 1)//self.batch_size
 from keras_tqdm import TQDMCallback
 
-def subblock(x, filter, **kwargs):
-    x = BatchNormalization()(x)
-    y = x
-    y = Conv2D(filter, (1, 1), activation='relu', **kwargs)(y) # Reduce the number of features to 'filter'
-    y = BatchNormalization()(y)
-    y = Conv2D(filter, (3, 3), activation='relu', **kwargs)(y) # Extend the feature field
-    y = BatchNormalization()(y)
-    y = Conv2D(K.int_shape(x)[-1], (1, 1), **kwargs)(y) # no activation # Restore the number of original features
-    y = Add()([x,y]) # Add the bypass connection
-    y = Activation('relu')(y)
-    return y
+from keras_model import build_model
 
-def build_model(lr, l2, activation='sigmoid'):
 
-    ##############
-    # BRANCH MODEL
-    ##############
-    regul  = regularizers.l2(l2)
-    optim  = Adam(lr=lr)
-    kwargs = {'padding':'same', 'kernel_regularizer':regul}
-
-    inp = Input(shape=img_shape) # 384x384x1
-    
-    '''
-    base_model = ResNet50(weights='imagenet', include_top=False)
-    for layer in base_model.layers:
-        layer.trainable = False
-    # 添加全局平均池化层
-    x = base_model.output
-    x = GlobalAveragePooling2D()(x)
-
-    # 添加一个全连接层
-    x = Dense(1024, activation='relu')(x)
-    x = Bidirectional(LSTM(1024, return_sequences=True), input_shape=(1024, 1))
-    branch_model = Model(inputs= base_model.input, outputs=x)
-
-    '''
-    #  branch model old  
-    x   = Conv2D(64, (9,9), strides=2, activation='relu', **kwargs)(inp)
-
-    x   = MaxPooling2D((2, 2), strides=(2, 2))(x) # 96x96x64
-    for _ in range(2):
-        x = BatchNormalization()(x)
-        x = Conv2D(64, (3,3), activation='relu', **kwargs)(x)
-
-    x = MaxPooling2D((2, 2), strides=(2, 2))(x) # 48x48x64
-    x = BatchNormalization()(x)
-    x = Conv2D(128, (1,1), activation='relu', **kwargs)(x) # 48x48x128
-    for _ in range(4): x = subblock(x, 64, **kwargs)
-
-    x = MaxPooling2D((2, 2), strides=(2, 2))(x) # 24x24x128
-    x = BatchNormalization()(x)
-    x = Conv2D(256, (1,1), activation='relu', **kwargs)(x) # 24x24x256
-    for _ in range(4): x = subblock(x, 64, **kwargs)
-
-    x = MaxPooling2D((2, 2), strides=(2, 2))(x) # 12x12x256
-    x = BatchNormalization()(x)
-    x = Conv2D(384, (1,1), activation='relu', **kwargs)(x) # 12x12x384
-    for _ in range(4): x = subblock(x, 96, **kwargs)
-
-    x = MaxPooling2D((2, 2), strides=(2, 2))(x) # 6x6x384
-    x = BatchNormalization()(x)
-    x = Conv2D(512, (1,1), activation='relu', **kwargs)(x) # 6x6x512
-    for _ in range(4): x = subblock(x, 128, **kwargs)
-    
-    x             = GlobalMaxPooling2D()(x) # 512
-  #  print('d ',x.shape)
-  #  x = Bidirectional(LSTM(x.shape[0], return_sequences=True), input_shape=x.shape)(x)
-    x = Reshape(( 512, 1))(x) 
-  #  print('d2 ',x.shape)
-  #  x = LSTM(512, return_sequences=False)(x)
-
-  #  print('d3 ',x.shape)
-    branch_model  = Model(inp, x)
-    ############
-    #lstm model
-    ############
-   # lstm_model = Sequential()
-   # lstm_model.add(Bidirectional(LSTM(20, return_sequences=True), input_shape=(1024, 1)))
-  #  lstm_model.add(TimeDistributed(Dense(1, activation='sigmoid')))
-  #  lstm_model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
-    ############
-    # HEAD MODEL
-    ############
-    mid        = 32
-    xa_inp     = Input(shape=branch_model.output_shape[1:])
-    xb_inp     = Input(shape=branch_model.output_shape[1:])
-    x1         = Lambda(lambda x : x[0]*x[1])([xa_inp, xb_inp])
-    x2         = Lambda(lambda x : x[0] + x[1])([xa_inp, xb_inp])
-    x3         = Lambda(lambda x : K.abs(x[0] - x[1]))([xa_inp, xb_inp])
-    x4         = Lambda(lambda x : K.square(x))(x3)
-    x          = Concatenate()([x1, x2, x3, x4])
-    x          = Reshape((4, branch_model.output_shape[1], 1), name='reshape1')(x)
-
-    # Per feature NN with shared weight is implemented using CONV2D with appropriate stride.
-    x          = Conv2D(mid, (4, 1), activation='relu', padding='valid')(x)
-    x          = Reshape((branch_model.output_shape[1], mid, 1))(x)
-    x          = Conv2D(1, (1, mid), activation='linear', padding='valid')(x)
-    x          = Flatten(name='flatten')(x)
-    
-    # Weighted sum implemented as a Dense layer.
-    x          = Dense(1, use_bias=True, activation=activation, name='weighted-average')(x)
-    head_model = Model([xa_inp, xb_inp], x, name='head')
-
-    ########################
-    # SIAMESE NEURAL NETWORK
-    ########################
-    # Complete model is constructed by calling the branch model on each input image,
-    # and then the head model on the resulting 512-vectors.
-    img_a      = Input(shape=img_shape)
-    img_b      = Input(shape=img_shape)
-    xa         = branch_model(img_a)
-    xb         = branch_model(img_b)
-    x          = head_model([xa, xb])
-    model      = Model([img_a, img_b], x)
-    model.compile(optim, loss='binary_crossentropy', metrics=['binary_crossentropy', 'acc'])
-    return model, branch_model, head_model
-
-  
-  
-
-model, branch_model, head_model = build_model(64e-5,0)
+model, branch_model, head_model = build_model(64e-5,0, img_shape)
 head_model.summary()
 branch_model.summary()
 model.summary()
@@ -606,12 +481,12 @@ def set_lr(model, lr):
 
 def get_lr(model):
     return K.get_value(model.optimizer.lr)
-steps      = 0
+steps = 0
 class LossHistory(keras.callbacks.Callback):
-    def on_epoch_end(self, batch, logs={}):
-        writer.add_scalar('Train/loc_loss', logs.get('loss'), steps)
-        writer.add_scalar('Train/acc', logs.get('acc'), steps)
-        steps += 1
+    def on_epoch_end(self, epoch, logs={}):
+        writer.add_scalar('Train/loc_loss', logs.get('loss'), epoch)
+        writer.add_scalar('Train/acc', logs.get('acc'), epoch)
+       # steps += 1
         
 def score_reshape(score, x, y=None):
     """
@@ -663,7 +538,7 @@ def make_steps(step, ampl):
     @param step Number of epochs to perform
     @param ampl the K, the randomized component of the score matrix.
     """
-    global w2ts, t2i, steps, features, score, histories
+    global w2ts, t2i, features,steps, score, histories
     
     # shuffle the training pictures
     #train 是w--hs list,过滤了只有一个样本的w
@@ -714,7 +589,7 @@ def make_steps(step, ampl):
   #      feature_heatmap = cv2.applyColorMap(feature_map.astype(np.uint8), cv2.COLORMAP_JET)
   #      writer.add_image('{}/{}'.format(i, steps), feature_heatmap, steps)
     
-  #  steps += step
+    steps += step
     
     # Collect history data
     history['epochs'] = steps
@@ -726,7 +601,7 @@ def make_steps(step, ampl):
 
 model_name = 'mpiotte-standard-'
 histories  = []
-steps      = 0
+#steps      = 0
 
 if False:
     tmp = keras.models.load_model('mpiotte-standard.model')
