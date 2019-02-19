054800fcaa2f39befda06fc6c78ccd8cd5efcc1c
rosaann <147429240@qq.com>
2019-02-19 11:41:07 +0800

    d

--- main_noscore.py
+++ main_noscore.py
@@ -537,8 +537,99 @@ def set_lr(model, lr):
 
 def get_lr(model):
     return K.get_value(model.optimizer.lr)
+def save_model(steps):
+    branch_model.save(str(steps) + 'mpiotte-standard_branch.model')
+    head_model.save(str(steps) + 'mpiotte-standard_header.model')  
+    model.save(str(steps) + 'mpiotte-standard_model.model')
+   
+    branch_model_json = branch_model.to_json()
+    with open(str(steps) + "branch_model.json", "w") as json_file:
+        json_file.write(branch_model_json)
+    branch_model.save_weights(str(steps) + "branch_model.h5") 
+
+    head_model_json = head_model.to_json()
+    with open(str(steps) + "head_model.json", "w") as json_file:
+        json_file.write(head_model_json)
+    head_model.save_weights(str(steps) + "head_model.h5") 
+
+    model_json = model.to_json()
+    with open(str(steps) + "main_model.json", "w") as json_file:
+        json_file.write(model_json)
+    model.save_weights(str(steps) + "main_model.h5") 
+
+
+#json_file = open('model.json', 'r')
+#loaded_model_json = json_file.read()
+#json_file.close()
+#loaded_model = model_from_json(loaded_model_json)
+#loaded_model.load_weights("model.h5")
+# Not computing the submission in this notebook because it is a little slow. It takes about 15 minutes on setup with a GTX 1080.
+import gzip
+
+def prepare_submission(known, threshold, filename):
+    """
+    Generate a Kaggle submission file.
+    @param threshold the score given to 'new_whale'
+    @param filename the submission file name
+    """
+    vtop  = 0
+    vhigh = 0
+    pos   = [0,0,0,0,0,0]
+    with gzip.open(filename, 'wt', newline='\n') as f:
+        f.write('Image,Id\n')
+        for i,p in enumerate(tqdm(submit)):
+            t = []
+            s = set()
+            a = score[i,:]
+            for j in list(reversed(np.argsort(a))):
+                h = known[j]
+                if a[j] < threshold and new_whale not in s:
+                    pos[len(t)] += 1
+                    s.add(new_whale)
+                    t.append(new_whale)
+                    if len(t) == 5: break;
+                for w in h2ws[h]:
+                    assert w != new_whale
+                    if w not in s:
+                        if a[j] > 1.0:
+                            vtop += 1
+                        elif a[j] >= threshold:
+                            vhigh += 1
+                        s.add(w)
+                        t.append(w)
+                        if len(t) == 5: break;
+                if len(t) == 5: break;
+            if new_whale not in s: pos[5] += 1
+            assert len(t) == 5 and len(s) == 5
+            f.write(p + ',' + ' '.join(t[:5]) + '\n')
+    return vtop,vhigh,pos
+
+def genSubFile(steps):
+    # Find elements from training sets not 'new_whale'
+    h2ws = {}
+    for p,w in tagged.items():
+        if w != new_whale: # Use only identified whales
+            h = p2h[p]
+            if h not in h2ws: h2ws[h] = []
+            if w not in h2ws[h]: h2ws[h].append(w)
+    known = sorted(list(h2ws.keys()))
+
+    # Dictionary of picture indices
+    h2i   = {}
+    for i,h in enumerate(known): h2i[h] = i
+
+    # Evaluate the model.
+    fknown  = branch_model.predict_generator(FeatureGen(known, img_shape), max_queue_size=20, workers=10, verbose=0)
+    fsubmit = branch_model.predict_generator(FeatureGen(submit, img_shape), max_queue_size=20, workers=10, verbose=0)
+    score   = head_model.predict_generator(ScoreGen(fknown, fsubmit), max_queue_size=20, workers=10, verbose=0)
+    score   = score_reshape(score, fknown, fsubmit)
+
+    # Generate the subsmission file.
+    prepare_submission(known, 0.99, str(steps) + '_zl_mpiotte-standard_mine.csv.gz')
+    
+
 steps = 0
-sub_index_list = [1, 140, 160, 220, 320, 400]
+sub_index_list = [0, 140, 160, 220, 320, 400]
 
 class LossHistory(keras.callbacks.Callback):
     def on_epoch_end(self, epoch, logs={}):
@@ -711,96 +802,6 @@ else:
     set_lr(model, 1e-5)
     for _ in range(2): make_steps(5, 0.25)
     
-    
-def save_model(steps):
-    branch_model.save(str(steps) + 'mpiotte-standard_branch.model')
-    head_model.save(str(steps) + 'mpiotte-standard_header.model')  
-    model.save(str(steps) + 'mpiotte-standard_model.model')
-   
-    branch_model_json = branch_model.to_json()
-    with open(str(steps) + "branch_model.json", "w") as json_file:
-        json_file.write(branch_model_json)
-    branch_model.save_weights(str(steps) + "branch_model.h5") 
-
-    head_model_json = head_model.to_json()
-    with open(str(steps) + "head_model.json", "w") as json_file:
-        json_file.write(head_model_json)
-    head_model.save_weights(str(steps) + "head_model.h5") 
-
-    model_json = model.to_json()
-    with open(str(steps) + "main_model.json", "w") as json_file:
-        json_file.write(model_json)
-    model.save_weights(str(steps) + "main_model.h5") 
-
-
-#json_file = open('model.json', 'r')
-#loaded_model_json = json_file.read()
-#json_file.close()
-#loaded_model = model_from_json(loaded_model_json)
-#loaded_model.load_weights("model.h5")
-# Not computing the submission in this notebook because it is a little slow. It takes about 15 minutes on setup with a GTX 1080.
-import gzip
-
-def prepare_submission(known, threshold, filename):
-    """
-    Generate a Kaggle submission file.
-    @param threshold the score given to 'new_whale'
-    @param filename the submission file name
-    """
-    vtop  = 0
-    vhigh = 0
-    pos   = [0,0,0,0,0,0]
-    with gzip.open(filename, 'wt', newline='\n') as f:
-        f.write('Image,Id\n')
-        for i,p in enumerate(tqdm(submit)):
-            t = []
-            s = set()
-            a = score[i,:]
-            for j in list(reversed(np.argsort(a))):
-                h = known[j]
-                if a[j] < threshold and new_whale not in s:
-                    pos[len(t)] += 1
-                    s.add(new_whale)
-                    t.append(new_whale)
-                    if len(t) == 5: break;
-                for w in h2ws[h]:
-                    assert w != new_whale
-                    if w not in s:
-                        if a[j] > 1.0:
-                            vtop += 1
-                        elif a[j] >= threshold:
-                            vhigh += 1
-                        s.add(w)
-                        t.append(w)
-                        if len(t) == 5: break;
-                if len(t) == 5: break;
-            if new_whale not in s: pos[5] += 1
-            assert len(t) == 5 and len(s) == 5
-            f.write(p + ',' + ' '.join(t[:5]) + '\n')
-    return vtop,vhigh,pos
-
-def genSubFile(steps):
-    # Find elements from training sets not 'new_whale'
-    h2ws = {}
-    for p,w in tagged.items():
-        if w != new_whale: # Use only identified whales
-            h = p2h[p]
-            if h not in h2ws: h2ws[h] = []
-            if w not in h2ws[h]: h2ws[h].append(w)
-    known = sorted(list(h2ws.keys()))
-
-    # Dictionary of picture indices
-    h2i   = {}
-    for i,h in enumerate(known): h2i[h] = i
-
-    # Evaluate the model.
-    fknown  = branch_model.predict_generator(FeatureGen(known, img_shape), max_queue_size=20, workers=10, verbose=0)
-    fsubmit = branch_model.predict_generator(FeatureGen(submit, img_shape), max_queue_size=20, workers=10, verbose=0)
-    score   = head_model.predict_generator(ScoreGen(fknown, fsubmit), max_queue_size=20, workers=10, verbose=0)
-    score   = score_reshape(score, fknown, fsubmit)
-
-    # Generate the subsmission file.
-    prepare_submission(known, 0.99, str(steps) + '_zl_mpiotte-standard_mine.csv.gz')
     
 
     
